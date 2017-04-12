2a39c7fcd668bb379e4afa4744aeb57d65495162
Pratheek Hegde <ptk609@gmail.com>
2017-04-12 15:35:17 +0530

    refactor: common module is exported from shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,19 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   declarations: [],
   exports: [
-    CommonModule, FormsModule, ReactiveFormsModule, RouterModule, HttpModule]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
--- src/app/vehicles/vehicles.module.ts
+++ src/app/vehicles/vehicles.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
 import { ClarityModule } from 'clarity-angular';
 
 import { SharedModule } from './../shared/shared.module';
@@ -18,9 +16,8 @@ import { VehicleRoutingModule }    from './vehicle-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule,
     ClarityModule.forChild(),
-    SharedModule.forRoot(),
+    SharedModule,
     VehicleRoutingModule
   ],
   declarations: [
